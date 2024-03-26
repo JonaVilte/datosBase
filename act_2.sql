3eb5a16f1ef4b14439457bc40383ffc7a7c74b04
Jonatan Vilte <togo36918@gmail.com>
2024-03-26 16:58:20 -0300

    acts.2  terminada hasta el act.2

---
+++ act_2.sql
@@ -0,0 +1,10 @@
+
+--- Ejercicio 1
+SELECT FirstName, LastName from employees
+ORDER by FirstName
+
+--- Ejercicio 2
+SELECT Title, ReportsTo FROM employees where ReportsTo is null
+
+--- Ejercicio 3
+
