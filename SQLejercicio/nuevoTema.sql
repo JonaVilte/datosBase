6e419ad8227cebd6f9f8eb80cb6ea9cdc71e3301
Jonatan <togo36918@gmail.com>
2024-06-25 15:09:17 -0300

    Hicimos un repaso para la prueva

--- SQLejercicio/nuevoTema.sql
+++ SQLejercicio/nuevoTema.sql
@@ -13,4 +13,26 @@ DELETE FROM genres WHERE GenreId in
 	HAVING count(s.TrackId) < 50
 )
 
------------------------
+-------------------------------------------------------------------------
+--Datos a tener en cuenta.
+----------------------------------------------------
+-----------Falta agregar el nombre de los manager
+SELECT first_name, manager_id ,salary, department_name, country_name FROM employees a
+JOIN departments d on a.department_id=d.department_id
+JOIN locations f on d.location_id=f.location_id
+JOIN countries e on f.country_id=e.country_id
+ORDER by salary
+
+
+-----------------------------------------------------------
+
+
+UPDATE jobs SET min_salary = min_salary*0.75, max_salary = max_salary*0.75
+---Remplazar un valor por su 25%de ese numero
+
+----------------------------------------------
+DELETE FROM employees WHERE salary in (
+SELECT salary FROM employees f
+JOIN jobs d on f.job_id = d.job_id
+WHERE salary > max_salary 
+)
